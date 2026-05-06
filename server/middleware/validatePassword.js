c8d139d8c476d4449f2441800d5f9b0483146918
vishnuuuvrdn <vishnuvardhan9376@gmail.com>
2026-05-06 13:30:48 +0530

    fix: multiple response error

--- server/middleware/validatePassword.js
+++ server/middleware/validatePassword.js
@@ -1,14 +1,13 @@
 const zxcvbn = require("zxcvbn");
 
-const validatePassword = (req, res) => {
+const validatePassword = (req, res, next) => {
     const { password } = req.body;
     const result = zxcvbn(password);
 
     if(result.score < 3){
         return res.status(400).json({ success: false, message: "ENTER STRONG PASSWORD!" });
     }
-
-    res.json({ success: true, message: "Password is strong enough!" });
+    next();
 }
 
 module.exports = validatePassword;
